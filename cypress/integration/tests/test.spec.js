3422260a18c051f604bf0fa2b16659699505b894
handesaurabh <handesaurabh2004@gmail.com>
2025-12-19 23:48:39 +0530

    Create clean test file with explicit selectors and force checks

--- cypress/integration/tests/test.spec.js
+++ cypress/integration/tests/test.spec.js
@@ -5,26 +5,21 @@ describe('Memory game', () => {
   });
 
   it('Validating different modes', () => {
+    // Test Easy mode
     cy.visit('http://localhost:8080');
-    
-    // Test Easy mode - directly check the radio button
-    cy.get('input#easy').should('exist').check();
+    cy.get('input#easy').should('exist').check({ force: true });
     cy.contains('Easy Mode').should('be.visible');
     cy.get('.tile').should('have.length', 8);
     
-    // Navigate away and back for a fresh start (instead of reset)
-    cy.visit('http://localhost:8080');
-    
     // Test Normal mode
-    cy.get('input#normal').should('exist').check();
+    cy.visit('http://localhost:8080');
+    cy.get('input#normal').should('exist').check({ force: true });
     cy.contains('Normal Mode').should('be.visible');
     cy.get('.tile').should('have.length', 16);
     
-    // Navigate away and back for a fresh start
-    cy.visit('http://localhost:8080');
-    
     // Test Hard mode
-    cy.get('input#hard').should('exist').check();
+    cy.visit('http://localhost:8080');
+    cy.get('input#hard').should('exist').check({ force: true });
     cy.contains('Hard Mode').should('be.visible');
     cy.get('.tile').should('have.length', 32);
   });
